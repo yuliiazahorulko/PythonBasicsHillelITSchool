86531bd484f24c4025e7f5908017b4d7f9f6b9b4
teacher <123qwe>
2024-11-22 13:26:12 +0200

    7 commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("Hello World!")
+
+a = 1234
+print( a // 1000)
+print( a // 100 - a // 1000 * 10)
+print( a // 10 - a // 100 * 10)
+print( a // 1 - a // 10 * 10)
+
+print ("----")
+b = 12345
+print( b % 10 )
+print( (b % 100 - b % 10)//10)
+print( (b % 1000 - b % 100)//100)
+print( (b % 10000 - b % 1000)//1000)
+print( (b % 100000 - b % 10000)//10000)
