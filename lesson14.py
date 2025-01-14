2d2c8091f47fff9c550d36fb0992509d7003869a
teacher <123qwe>
2025-01-14 21:09:49 +0200

    23 commit

---
+++ lesson14.py
@@ -0,0 +1,42 @@
+from my_package import math_utils, string_utils
+from my_package.subpackage import advanced_utils
+# from my_package.math_utils import *
+# from my_package.string_utils import *
+
+
+print(math_utils.add(5, 3))
+print(string_utils.to_upper("hi"))
+print(advanced_utils.square(4))
+print(math_utils.__d)
+
+
+print(string_utils.e)
+print(string_utils._f)
+print(string_utils.__g)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(add(2, 4))
+# print(__c)
+# print(multiply(1, 2))
+# print(__d)
+#
+# print(e)
+# print(_f)
+# print(string_utils.__g)
+
+
---
+++ my_package/string_utils.py
@@ -0,0 +1,8 @@
+def to_upper(s):
+    return s.upper()
+
+
+e = 5
+_f = 6
+__g = 7
+
